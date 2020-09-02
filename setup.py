b30d947ec17cacb297618382649d7379760da194
chiang9 <jchiang1225@gmail.com>
2020-09-02 15:43:49 +0800

    pre-release version1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='polygon_crop',
-    version='0.0.1',
+    version='0.0.2',
     description = 'An image processing package which supports interactive cropping function',
     packages=[
         'polygon_crop'
